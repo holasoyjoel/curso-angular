b98212237f6cf0443ab44d49570321d87f02fa9b
Correa Alejandro Joel <joelcorrea.alejandro123@gmail.com>
2022-06-29 20:55:01 -0300

    04-tarea.tipado.ts agregado

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
 /*
     ===== Código de TypeScript =====
 */
-function sumarFuncion(a, b) {
-    return a + b;
-}
-function multiplicar(numero, base, otroNumero) {
-    if (base === void 0) { base = 2; }
-    return numero * base;
-}
-function curar(personaje, curarX) {
-    personaje.pv += curarX;
-}
-var nuevoPersonaje = {
-    nombre: 'Strider',
-    pv: 50,
-    mostrarHp: function () {
-        console.log('Puntos de vida:', this.pv);
-    }
-};
-curar(nuevoPersonaje, 20);
-nuevoPersonaje.mostrarHp();
