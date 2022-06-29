278ccb30fe6e0effce757d49b9cb8452122fcf23
Correa Joel <joelcorrea.alejandro123@gmail.com>
2022-06-29 13:04:46 -0300

    02-array-objeto.ts agregado

---
+++ ejercicios/02-array-objetos.ts
@@ -0,0 +1,24 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let habilidades: string[] = ['Bash' , 'Counter' , 'Healing'];
+
+
+interface Personaje {
+    nombre: string,
+    hp: number,
+    habilidades: string[],
+    pueblo?: string
+};
+
+
+const personaje: Personaje = {
+    nombre: 'Strider',
+    hp: 100,
+    habilidades: ['Bash' , 'Counter' , 'Healing']
+};
+
+personaje.pueblo = 'Pueblo Paleta';
+
+console.table(personaje);
